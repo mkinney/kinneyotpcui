d66a83e5776b2296755612e543cb26e72a48fbae
Mike Kinney <mike.kinney@gmail.com>
2024-01-08 11:38:10 -0800

    got the encode form working

--- otp.py
+++ otp.py
@@ -1,10 +1,9 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Footer, Label, Markdown, TabbedContent, TabPane
+from textual.widgets import Footer, Input, Label, Markdown, TabbedContent, TabPane
+from textual.widget import Widget
+from textual.reactive import reactive
 
-# TODO: encode form
-ENCODE = """
-Encode form goes here
-"""
+from kinneyotp import OTP
 
 # TODO: decode form
 DECODE = """
@@ -32,8 +31,10 @@ Tips:
 - Consider adding (or using) a character (or phrase) that indicates that the message was sent under duress.
 """
 
-class OTP(App):
+class Form(App):
     """One Time Pad"""
+
+    otp = OTP()
 
     # Note: Using numbers to switch tabs quickly since the default alphabet does not contain numbers.
     BINDINGS = [
@@ -42,8 +43,12 @@ class OTP(App):
         ("3", "show_tab('generate')", "Generate"),
         ("4", "show_tab('settings')", "Settings"),
         ("5", "show_tab('about')", "About"),
-        ("q", "quit", "Quit"),
+        ("6", "quit", "Quit"),
     ]
+
+
+    def render(self) -> str:
+        return f"Hello, {self.who}!"
 
     def compose(self) -> ComposeResult:
         """Compose app with tabbed content."""
@@ -52,8 +57,11 @@ class OTP(App):
 
         # Add the TabbedContent widget
         with TabbedContent(initial="encode"):
-            with TabPane("Encode", id="encode"):  # First tab
-                yield Markdown(ENCODE)  # Tab content
+            with TabPane("Encode", id="encode"):
+                yield Input(placeholder="Text", id="text")
+                yield Input(placeholder="Key", id="key")
+                yield Input(placeholder="Encoded", disabled=True, id="encoded")
+                yield Input(placeholder="", disabled=True, id="message")
             with TabPane("Decode", id="decode"):
                 yield Markdown(DECODE)
             with TabPane("Generate", id="generate"):
@@ -63,11 +71,30 @@ class OTP(App):
             with TabPane("About", id="about"):
                 yield Markdown(ABOUT)
 
+    def update_encoded(self):
+        # Update the encoded value
+        text = self.query_one("#text")
+        key = self.query_one("#key")
+        encoded = self.query_one("#encoded")
+        message = self.query_one("#message")
+
+        if len(text.value) <= len(key.value):
+            self.otp.key = key.value
+            message.value, encoded_text = self.otp.encode(text.value)
+            encoded.value = encoded_text
+        else:
+            message.value = "The length of the text must be shorter or the same length as the key."
+            encoded.value = ""
+
+    def on_input_changed(self, event: Input.Changed) -> None:
+        if event.input.id == "text" or event.input.id == "key":
+            self.update_encoded()
+
     def action_show_tab(self, tab: str) -> None:
         """Switch to a new tab."""
         self.get_child_by_type(TabbedContent).active = tab
 
 
 if __name__ == "__main__":
-    app = OTP()
+    app = Form()
     app.run()
--- readme.txt
+++ readme.txt
@@ -1,6 +1,6 @@
 pythong3 -m venv venv
 act
 pip install --upgrade pip
-pip install textual
+pip install textual kinneyotp
 
 python otp
